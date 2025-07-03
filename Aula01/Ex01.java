661eb14eec2145797762ab65eb2f578d88eb2b70
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2025-07-03 16:13:45 -0300

    Add files via upload

---
+++ Aula01/Ex01.java
@@ -0,0 +1,6 @@
+public class Ex1 {
+    public static void main(String[] args) { /* */
+        //Exibe a saída de dados
+        System.out.println("Java Foundations");
+    }
+}
---
+++ Aula01/Ex02.java
@@ -0,0 +1,12 @@
+/*
+* Exemplo de uma declaração usando variável
+* */
+public class Ex2 {
+    public static void main(String[] args) {
+    //Declarando uma variável de tipo inteiro
+        int numero = 3;
+
+        System.out.println("O valor da variável é: " + numero);
+
+    }
+}
